4cd2b5cf163ff318db6cd59c65af34ac88d88e82
Roger Drakes <drakescodes@gmail.com>
2023-05-25 12:30:19 -0400

    work added

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// import NotesView from "./NotesView.js";
+// import NotesAPI from "./NotesAPI.js";
+import App from "./App.js";
+const root = document.getElementById("app");
+const app = new App(root);
+// const view = new NotesView(app, {
+//   onNoteAdd() {
+//     console.log("lets add note");
+//   },
+//   onNoteSelect(id) {
+// console.log(`note ${id} selected`);
+
+//   },
+//   onNoteEdit(newTitle, newBody) {
+//     console.log(newTitle, newBody);
+
+//   },
+//   onNoteDelete(id) {
+//     console.log(`note ${id} deleted`);
+
+//   }
+
+// });
+// const notes = NotesAPI.getAllNotes();
+// view.updateNoteList(notes);
+// view.updatesActiveNote(notes[0]);
